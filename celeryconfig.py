51bac690e87fc1c273f524b01e65c2bb0303a238
skhristoforov <stslv91@gmail.com>
2017-07-27 12:55:57 +0300

    update

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,21 +2,23 @@ from kombu import Queue
 
 broker_url = 'pyamqp://myuser:mypassword@localhost/myvhost'
 result_backend = 'redis://localhost'
-
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
 timezone = 'Europe/Moscow'
 enable_utc = True
 
+#  INCLUDING ALL KINDS OF TASKS
 include = ['MID_tasks', 'TSM_tasks']
+#  CREATING QUEUES
 task_queues = [
-    Queue('Queue.MID', routing_key='MID'),
-    Queue('Queue.TSM', routing_key='TSM'),
+    Queue('Queue.MID', routing_key='priority.#'),
+    Queue('Queue.TSM', routing_key='priority.#'),
 ]
-
-task_default_routing_key = 'medium'
+#  DEFINING ROUTES FOR DIFFERENT KINDS OF TASKS
 task_routes = {
     'MID_tasks.*': {'queue': 'Queue.MID'},
     'TSM_tasks.*': {'queue': 'Queue.TSM'}
 }
+
+task_default_routing_key = 'priority.medium'
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from remote import remote
+# from remote import remote
+#
+# MID_result = remote.send_task('MID_tasks.hello_MID',  args=[1, 2], kwargs={})
+# TSM_result = remote.send_task('TSM_tasks.hello_TSM',  args=[3, 4], kwargs={})
 
-MID_result = remote.send_task('MID_tasks.hello_MID',  args=[1, 2], kwargs={})
-TSM_result = remote.send_task('TSM_tasks.hello_TSM',  args=[3, 4], kwargs={})
+from MID_tasks import hello_MID
+from TSM_tasks import hello_TSM
 
-MID_result.get(timeout=1 * 60)
-TSM_result.get(timeout=1 * 60)
+MID_result = hello_MID.delay(1, 2)
+TSM_result = hello_TSM.delay(3, 4)
+
+print(MID_result.get(timeout=1 * 60))
+print(TSM_result.get(timeout=1 * 60))
 
