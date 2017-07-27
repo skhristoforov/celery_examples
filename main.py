d1df2d5c3e3c414d756cf2641f61edb7bc0b8f56
skhristoforov <stslv91@gmail.com>
2017-07-27 16:58:39 +0300

    update

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from MID_tasks import hello_MID_hiprior
 from TSM_tasks import hello_TSM
 
 MID_result_hi, MID_result_low, TSM_result = [], [], []
-for t in range(10):
+for t in range(100):
     MID_result_low.append(hello_MID_lowprior.delay(1, 2))
     TSM_result.append(hello_TSM.delay(3, 4))
 
-for t in range(10):
+for t in range(100):
     MID_result_hi.append(hello_MID_hiprior.delay(1, 2))
     MID_result_low.append(hello_MID_lowprior.delay(1, 2))
     TSM_result.append(hello_TSM.delay(3, 4))
 
-for t in range(20):
+
+for t in range(100):
     print(MID_result_hi[t].get(timeout=1 * 60))
+for t in range(200):
     print(MID_result_low[t].get(timeout=1 * 60))
     print(TSM_result[t].get(timeout=1 * 60))
 
