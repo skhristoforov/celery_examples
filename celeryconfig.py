9beb93aa5a5538f945917ff72f3e05b4cb1f339a
skhristoforov <stslv91@gmail.com>
2017-07-27 12:37:25 +0300

    update

--- celeryconfig.py
+++ celeryconfig.py
@@ -11,11 +11,11 @@ enable_utc = True
 
 include = ['MID_tasks', 'TSM_tasks']
 task_queues = [
-    Queue('Queue.MID', routing_key='priority.#'),
-    Queue('Queue.TSM', routing_key='priority.#'),
+    Queue('Queue.MID', routing_key='MID'),
+    Queue('Queue.TSM', routing_key='TSM'),
 ]
 
-task_default_routing_key = 'priority.medium'
+task_default_routing_key = 'medium'
 task_routes = {
     'MID_tasks.*': {'queue': 'Queue.MID'},
     'TSM_tasks.*': {'queue': 'Queue.TSM'}
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from remote import remote
 
 MID_result = remote.send_task('MID_tasks.hello_MID',  args=[1, 2], kwargs={})
-TSM_result = remote.send_task('TSM_tasks.hello_TSM', args=[3, 4], kwargs={})
+TSM_result = remote.send_task('TSM_tasks.hello_TSM',  args=[3, 4], kwargs={})
 
 MID_result.get(timeout=1 * 60)
 TSM_result.get(timeout=1 * 60)
