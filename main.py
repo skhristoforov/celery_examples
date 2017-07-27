7b005e5efc5ad98b8cc2f865444d244842043d60
skhristoforov <stslv91@gmail.com>
2017-07-27 10:28:11 +0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from remote import remote
 
-MID_result = app.send_task('MID_tasks.add', args=[1, 2], kwargs={})
+MID_result = remote.send_task('MID_tasks.add', args=[1, 2], kwargs={})
 print(MID_result.get(timeout=1 * 60))
 
-MID_result = app.send_task('TSM_tasks.prod', args=[3, 4], kwargs={})
+MID_result = remote.send_task('TSM_tasks.prod', args=[3, 4], kwargs={})
 print(MID_result.get(timeout=1 * 60))
 
