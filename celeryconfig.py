f73ea417add64d63f311eae75a48e1bf5b4da827
skhristoforov <stslv91@gmail.com>
2017-07-27 12:23:06 +0300

    update

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,3 +1,5 @@
+from kombu import Queue
+
 broker_url = 'pyamqp://myuser:mypassword@localhost/myvhost'
 result_backend = 'redis://localhost'
 
@@ -6,9 +8,15 @@ result_serializer = 'json'
 accept_content = ['json']
 timezone = 'Europe/Moscow'
 enable_utc = True
+
 include = ['MID_tasks', 'TSM_tasks']
-task_queues = ['priority.hi', 'priority.medium', 'priority.low']
+task_queues = [
+    Queue('Queue.MID', routing_key='priority.#'),
+    Queue('Queue.TSM', routing_key='priority.#'),
+]
+
+task_default_routing_key = 'priority.medium'
 task_routes = {
-    'MID_tasks.hello_MID': {'queue': 'priority.hi'},
-    'MID_tasks.hello_TSM': {'queue': 'priority.medium'}
+    'MID_tasks.*': {'queue': 'Queue.MID'},
+    'TSM_tasks.*': {'queue': 'Queue.TSM'}
 }
